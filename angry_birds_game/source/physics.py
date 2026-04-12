7f343021af6d88597c6d31ee1122ed84a3df83a9
sazidludi <sazidludi@gmail.com>
2026-04-11 21:34:27 -0400

    feat: add future_pos for tracjecotry

--- angry_birds_game/source/physics.py
+++ angry_birds_game/source/physics.py
@@ -10,3 +10,14 @@ def update(bird):
     #pos based on velocity
     bird.x += bird.vx 
     bird.y += bird.vy 
+
+#future position
+# **TODO**: Mira: use this and add UI that shows the trajectory
+def future_pos(bird, frames_ahead): 
+    x, y = bird.x, bird.y
+    vx, vy = bird.vx, bird.vy
+    for _ in range(frames_ahead):
+        vy += GRAVITY
+        x += vx
+        y += vy
+    return x, y
