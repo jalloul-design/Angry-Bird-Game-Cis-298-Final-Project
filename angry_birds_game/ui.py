d8f0b112daafac7e4b086748756563e686031b76
jalloul-design <jalloul@umich.edu>
2026-04-14 01:23:56 -0400

    Fixing UI file, Fixed the test = .. line to match the setting.py file

--- angry_birds_game/ui.py
+++ angry_birds_game/ui.py
@@ -1,7 +1,9 @@
 # Owned By Mira
 import pygame
+from settings import COLOR_UI_TEXT, SCREEN_WIDTH
 
 def draw_menu(screen):
-    font = pygame.font.Font(None,50)
-    text = font.render("Welcome to Angry Birds!!", True, (255, 255, 255))
-    screen.blit(text, (250, 200))
+    font = pygame.font.Font(None, 50)
+    text = font.render("Welcome to Angry Birds!!", True, COLOR_UI_TEXT)
+    x = (SCREEN_WIDTH - text.get_width()) // 2
+    screen.blit(text, (x, 200))
